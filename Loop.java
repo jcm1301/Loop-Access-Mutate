8df2b8aa8b8e645d9a6ebbd5c1bbb660e8088edc
James Morse <113059074+jcm1301@users.noreply.github.com>
2022-09-17 13:56:34 -0400

    Add files via upload

--- Loop.java
+++ Loop.java
@@ -2,6 +2,9 @@
 //import java.io.IOException;
 import java.util.Scanner;
 
+
+//Write class header, intialize variables and attributes
+
 public class Loop {
   
     int firstNum; 
@@ -9,47 +12,56 @@ public class Loop {
     int thirdNum;
     int fourthNum; 
     
+
+    //Write constructor signature
    public Loop(){
         
    }
 
+   //Write method signature with user input parameter 
     public void loop(Scanner input){
-
-    //public static void main(String[] args){
     
         System.out.println("");
         System.out.println("Please enter a number: ");
        
-        int firstNum = input.nextInt();
+        //Assign user input to variable
+        int userNum = input.nextInt();
         
         input.close();
 
+        //for loop with 5 iterations of calculations
         for(int i = 0; i<5; i++){
             
             System.out.println("");
             System.out.println("i: " + i);
 
-            firstNum += i + 25;
+            //adds iteration number to 25
+            firstNum = i + userNum + 25;
             
-            System.out.println("i + 25: " + firstNum);
+            System.out.println("i + " + userNum + " + 25 = " + firstNum);
 
+            //divides that number by 3 and finds remainder
             secondNum = firstNum % 3; 
 
-            System.out.println("modulus 3: " + secondNum);
+            System.out.println("remainder of that number divided by 3: " + secondNum);
 
+            //adds those two numbers
             thirdNum = firstNum + secondNum;
+
             System.out.println(firstNum + " + " + secondNum + " = " + thirdNum);
 
+            //adds that sum to each subsequent sum 
             fourthNum = fourthNum + thirdNum; 
-            //sumThirdNums = thirdNum; 
+    
 
             i = i++; 
             
-            //return thirdNum;  
+            //resets first calculation to 0 for next loop
+            firstNum = 0; 
+            
 
         }
         
-       //return thirdNum; 
   }
 
     
