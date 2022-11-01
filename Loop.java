3809772c43351de91a9f55ad4116de0310206d0b
James Morse <113059074+jcm1301@users.noreply.github.com>
2022-11-01 15:45:50 -0400

    Add files via upload

--- Loop.java
+++ Loop.java
@@ -10,16 +10,36 @@ public class Loop {
     int firstNum; 
     int secondNum; 
     int thirdNum;
-    int fourthNum; 
-    
+    int fourthNum;
+    int fifthNum; 
+
+    //int x = 7;
+    //double y = 582.0;
+    //double z = 4.0;
+
+   //private int fifthNum = 256; 
 
     //Write constructor signature
    public Loop(){
-        
+        fifthNum = 0;
+
    }
+
+   public Loop(int fifthNum){
+      
+      fifthNum = 0; 
+
+   }
+
 
    //Write method signature with user input parameter 
     public void loop(Scanner input){
+
+       
+
+        //z = y * 4 - (15 % x + z);
+
+        //y = (5 + y)*x; 
     
         System.out.println("");
         System.out.println("Please enter a number: ");
@@ -27,43 +47,60 @@ public class Loop {
         //Assign user input to variable
         int userNum = input.nextInt();
         
-        input.close();
+        //input.close();
+
+        //if statement determining whether to run the loop or not
+        if (userNum >= 0) {
 
         //for loop with 5 iterations of calculations
-        for(int i = 0; i<5; i++){
+            for(int i = 0; i<5; i++){
             
-            System.out.println("");
-            System.out.println("i: " + i);
+                System.out.println("");
+                System.out.println("i: " + i);
 
-            //adds iteration number to 25
-            firstNum = i + userNum + 25;
+                //adds iteration number to 25
+                firstNum = i + userNum + 25;
             
-            System.out.println("i + " + userNum + " + 25 = " + firstNum);
+                System.out.println("i + " + userNum + " + 25 = " + firstNum);
 
-            //divides that number by 3 and finds remainder
-            secondNum = firstNum % 3; 
+                //divides that number by 3 and finds remainder
+                secondNum = firstNum % 3; 
 
-            System.out.println("remainder of that number divided by 3: " + secondNum);
+                System.out.println("remainder of that number divided by 3: " + secondNum);
 
-            //adds those two numbers
-            thirdNum = firstNum + secondNum;
+                //adds those two numbers
+                thirdNum = firstNum + secondNum;
 
-            System.out.println(firstNum + " + " + secondNum + " = " + thirdNum);
+                System.out.println(firstNum + " + " + secondNum + " = " + thirdNum);
 
-            //adds that sum to each subsequent sum 
-            fourthNum = fourthNum + thirdNum; 
+                //adds that sum to each subsequent sum 
+                fourthNum = fourthNum + thirdNum; 
     
 
-            i = i++; 
+                i = i++; 
             
-            //resets first calculation to 0 for next loop
-            firstNum = 0; 
-            
+                //resets first calculation to 0 for next loop
+                firstNum = 0; 
+
+                //z = z + 1; 
+
+                }
+
+                //System.out.println(y);
+                
+                //fifthNum += 2; 
 
         }
-        
+
+        //otherwise, print following message
+        else {
+            System.out.println("This program doesn't compute numbers below 0");
+            
+        }
+       
+
+    }
+
   }
-
     
-}
-
+    
--- LoopTest.java
+++ LoopTest.java
@@ -1,19 +1,56 @@
-//import java.io.Loop;
 //import java.io.IOException;
 import java.util.Scanner;
 
 public class LoopTest {
     
     //int firstNum; 
+    
+    
     public static void main(String[] args){
        
+        //Instantiate object
         Loop myLoop = new Loop();
         Scanner input = new Scanner(System.in);
 
+        //-----------------------Loop Class
+        
+        //call loop method with first user input and run loop
         myLoop.loop(input);
 
         System.out.println("");
         System.out.println("Sum of all sums: " + myLoop.fourthNum);
+        System.out.println("");
+
+        //------------------------AccessMutate Class
+
+        //Instantiate object 
+        AccessMutate Test1 = new AccessMutate(0, "initial String"); 
+        
+        //Access the initial value of variable fifthNum
+        System.out.println(Test1.access()); 
+        System.out.println("");
+
+        //Access initial value of variable fifthNum by printing object and variable
+        System.out.println(Test1.fifthNum);
+        System.out.println("");
+        
+        //Prompts user to enter new value for fifthNum
+        Scanner input2 = new Scanner(System.in);
+
+        //outputs this new value of fifthNum
+        System.out.println(Test1.mutate(input2));
+        System.out.println("");
+
+       
+        //Access the intial string userString1
+        System.out.println(Test1.accessString()); 
+        System.out.println("");
+        
+        //Prompts user to enter a new string
+        Scanner input3 = new Scanner(System.in);
+
+        //outputs this string
+        System.out.println(Test1.mutateString(input3));
         System.out.println("");
 
     }
